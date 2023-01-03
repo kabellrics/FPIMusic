2e95d6ffad4fdee4b5e9ca5dc15dfd06d647fddf
kabellrics <yflechel@gmail.com>
2023-01-03 23:24:35 +0100

    Create Extended Object

---
+++ FPIMusic.Services/Deezer/ExtendeObject/DeezerExtendedArtiste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPIMusic.Services.Deezer.ExtendeObject
+{
+    public class DeezerExtendedArtiste
+    {
+    }
+}
---
+++ FPIMusic.Services/Mediatheque/ExtendedObject/MediaExtendedArtiste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPIMusic.Services.Mediatheque.ExtendedObject
+{
+    public class MediaExtendedArtiste
+    {
+    }
+}
